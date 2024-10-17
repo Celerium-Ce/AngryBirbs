617cc07044c6f762581c0ec9a04df91f6c929ca6
Celerium-Ce <mohammad.umardarkonme@gmail.com>
2024-10-17 10:29:58 +0530

    Made load thingy now working on levelsMenu

---
+++ core/src/main/java/io/github/angrybirbs/menu/LevelsMenu.java
@@ -0,0 +1,52 @@
+package io.github.angrybirbs.menu;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import io.github.angrybirbs.Main;
+
+public class LevelsMenu extends Menu {
+    private TextureRegion bgTextureReigon;
+    private TextureRegionDrawable bgDrawable;
+    private Image bgImage;
+    private ImageButton back;
+    public LevelsMenu(Main game) {
+        super(game);
+        backgroundTexture = new Texture(Gdx.files.internal("MainMenuBG.png"));
+        bgTextureReigon = new TextureRegion(backgroundTexture);
+        bgDrawable = new TextureRegionDrawable(bgTextureReigon);
+        bgImage = new Image(bgDrawable);
+        bgImage.setFillParent(true);
+        bgImage.setZIndex(0);
+        stage.addActor(bgImage);
+
+        back = new ImageButton(
+            new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("Buttons/back.png")))),
+            new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("Buttons/back.png"))))
+        );
+
+        back.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeListener.ChangeEvent event, Actor actor) {
+                game.setScreen(new MainMenu(game));
+                dispose();
+            }
+        });
+
+        back.setSize(Gdx.graphics.getWidth()/10f, Gdx.graphics.getHeight()/10f);
+
+        back.setPosition(
+            0 - Gdx.graphics.getWidth()/50f,
+            0
+        );
+
+        stage.addActor(back);
+
+
+    }
+}
--- core/src/main/java/io/github/angrybirbs/menu/LoadMenu.java
+++ core/src/main/java/io/github/angrybirbs/menu/LoadMenu.java
@@ -4,13 +4,13 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.Align;
 import io.github.angrybirbs.Main;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
+
 
 import java.io.File;
 import java.util.ArrayList;
@@ -21,7 +21,7 @@ public class LoadMenu extends Menu{
     private TextureRegionDrawable bgDrawable;
     private Image bgImage;
     private ImageButton back;
-    private ArrayList<ImageButton> saves;
+    private ArrayList<ImageTextButton> saves;
     private int saveFileCount = 0;
 
     public LoadMenu(Main game) {
@@ -56,26 +56,35 @@ public class LoadMenu extends Menu{
 
         stage.addActor(back);
 
-        saves = new ArrayList<ImageButton>();
+        saves = new ArrayList<ImageTextButton>();
 
         File saveDataDir = new File(Gdx.files.local("SaveData").file().getAbsolutePath());
         File[] saveFiles = saveDataDir.listFiles((dir, name) -> name.endsWith(".bat"));
 
         if (saveFiles != null) {
+            Skin skin = new Skin(Gdx.files.internal("skin/cloud-form-ui.json"));
             Table table = new Table();
             table.setFillParent(true);
             stage.addActor(table);
 
             for (File file : saveFiles) {
-                ImageButton saveButton = new ImageButton(
-                    new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("Buttons/back.png")))),
-                    new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("Buttons/back.png"))))
-                );
+                ImageTextButton.ImageTextButtonStyle style = new ImageTextButton.ImageTextButtonStyle();
+                style.font = skin.getFont("title");
+                style.imageUp = new TextureRegionDrawable(new Texture(Gdx.files.internal("Buttons/empty.png")));
 
+                ImageTextButton saveButton = new ImageTextButton(file.getName().substring(0, file.getName().indexOf(".")), style);
+
+                Table buttonTable = new Table();
+                buttonTable.add(saveButton.getImage()).expand().bottom().row();
+                buttonTable.add(saveButton.getLabel()).expand().center().padBottom(10f);
+
+                saveButton.clearChildren();
+                saveButton.add(buttonTable).expand().fill();
+
+                // Listener for loading save
                 saveButton.addListener(new ChangeListener() {
                     @Override
                     public void changed(ChangeEvent event, Actor actor) {
-                        // Handle save button click
                         System.out.println("Loading save: " + file.getName());
                     }
                 });
@@ -87,7 +96,6 @@ public class LoadMenu extends Menu{
                     table.row();
                 }
             }
-
         }
         else {
             ImageButton noSaves = new ImageButton(
